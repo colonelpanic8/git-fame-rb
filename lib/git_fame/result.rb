0f01763ae7563d2b35a3a56def81dacb395d30f9
Ivan Malison <IvanMalison@gmail.com>
2024-10-09 16:33:50 -0600

    More hierarchichal

--- lib/git_fame/result.rb
+++ lib/git_fame/result.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 module GitFame
   class Result < Base
     attribute :contributions, Types.Array(Contribution)
@@ -27,13 +25,21 @@ module GitFame
     def lines
       contributions.sum(&:lines)
     end
+
     # @return [Hash<String, Hash<String, Integer>>]
-    # Returns a hash where each key is an author's email, and each value is another hash mapping file paths to line counts.
+    # Returns a hash where each key is an author's email, and each value is another hash mapping file paths (including subdirectories) to line counts.
     def lines_by_file
       contributions.each_with_object(Hash.new { |h, k| h[k] = Hash.new(0) }) do |contribution, result|
         author_email = contribution.author[:email]
         contribution.lines_by_file.each do |file, loc|
-          result[author_email][file] += loc
+          # Split the file path into its directories
+          path_parts = file.split(File::SEPARATOR)
+
+          # Iterate over each part to build paths for subdirectories
+          (1..path_parts.size).each do |i|
+            sub_path = path_parts.first(i).join(File::SEPARATOR)
+            result[author_email][sub_path] += loc
+          end
         end
       end
     end
